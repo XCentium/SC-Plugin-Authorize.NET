e2aae906e0608b1a3abe104bd655f27942c0651b
Richard.Hein <Richard.Hein@RICHARDHEIN-XC>
2017-06-12 16:05:44 -0400

    Add project files.

---
+++ XCentium.Plugins.Authorize.Net/ConfigureSitecore.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigureSitecore.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2017
+// </copyright>
+// <summary>
+//   The SamplePlugin startup class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace XCentium.Plugins.Authorize.Net
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore.Framework.Configuration;
+
+    /// <summary>
+    /// The carts configure sitecore class.
+    /// </summary>
+    public class ConfigureSitecore : IConfigureSitecore
+    {
+        /// <summary>
+        /// The configure services.
+        /// </summary>
+        /// <param name="services">
+        /// The services.
+        /// </param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.RegisterAllPipelineBlocks(assembly);
+
+            services.Sitecore().Pipelines(config => config
+             .AddPipeline<SamplePipeline, SamplePipeline>(
+                    configure =>
+                        {
+                            configure.Add<SampleBlock>();
+                        })
+            );
+
+            services
+                .AddScoped<SampleCommand, SampleCommand>();
+        }
+    }
+}
---
+++ XCentium.Plugins.Authorize.Net/Pipelines/ISamplePipeline.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ISamplePipeline.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2017
+// </copyright>
+// <summary>
+//   ISamplePipeline pipeline interface definition.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace XCentium.Plugins.Authorize.Net
+{
+    using Sitecore.Commerce.Core;
+    using Sitecore.Framework.Pipelines;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines the ISamplePipeline interface
+    /// </summary>
+    /// <seealso cref="Sitecore.Framework.Pipelines.IPipeline{XCentium.Plugins.Authorize.Net.SampleArgument, XCentium.Plugins.Authorize.Net.SampleEntity, Sitecore.Commerce.Core.CommercePipelineExecutionContext}" />
+    [PipelineDisplayName("SamplePipeline")]
+    public interface ISamplePipeline : IPipeline<SampleArgument, SampleEntity, CommercePipelineExecutionContext>
+    {
+    }
+}
